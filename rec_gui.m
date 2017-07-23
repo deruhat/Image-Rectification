bec6868262453369a85c12cafa58667a73686cee
Abdulellah Abualshour <delohat@live.com>
2017-07-23 19:03:09 +0300

    Update rec_gui.m

--- rec_gui.m
+++ rec_gui.m
@@ -161,64 +161,65 @@ Lall = L;
 angle_threshold=20;
 
 %% VERTICAL PERSPECTIVE
-%segment endpoints
+% segment endpoints
 X1=L(:,1); X2=L(:,3); Y1=L(:,2); Y2=L(:,4);
 XY=[X2-X1 Y2-Y1];
 
-%segment orientations
+% segment orientations
 lineangle =(atan(XY(:,2)./XY(:,1)))/pi*180;
 lineangle(lineangle<0)=lineangle(lineangle<0)+180;
-
 L(lineangle<angle_threshold | lineangle>(180-angle_threshold),:)=[];
-
 midpoints = (L(:,1:2)+L(:,3:4))/2;
 
-%segment endpoints
+% segment endpoints
 X1=L(:,1); X2=L(:,3); Y1=L(:,2); Y2=L(:,4);
 XY=[X2-X1 Y2-Y1];
 
-%segment orientations
+% segment orientations
 lineangle =(atan(XY(:,2)./XY(:,1)))/pi*180;
 lineangle(lineangle<0)=lineangle(lineangle<0)+180;
-
 data = [midpoints(:,1)  lineangle]';
 
+% RANSAC
 iterations = 20; % k
 min_points = 2; % n
 threshold = 4; % t
 num_inliers_required = 140; % d
-
-% initialization
 num_inliers_best = 0;
 x = data(1,:);
 y = data(2,:);
 
-% main loop
 for i=1:iterations
     
     % randomly select points from data (subset)
     [~,idx] = datasample(x,min_points);
+    
     % get coffecients
     coef = polyfit(x(1,idx),y(1,idx),1); % uses least squares
+    
     % get predictions of y from coefficients using polyval
     y_pred = polyval(coef,x);
+    
     % find square residual
     square_resid = (y - y_pred).^2;
+    
     % compare all points (using square residual) to threshold
     inliers = [x(square_resid < threshold); y(square_resid < threshold)];
     inliers_x1 = X1(square_resid < threshold);
     inliers_x2 = X2(square_resid < threshold);
     inliers_y1 = Y1(square_resid < threshold);
     inliers_y2 = Y2(square_resid < threshold);
+    
     % skip iteration if size of current inliers < size of best
     if length(inliers) < num_inliers_best
         continue;
     end
+    
     % current random sample is best sample if reached here
     best_inliers = inliers;
-    
     num_inliers_best = length(best_inliers);
     best_model = coef;
+    
     % break out of the loop if we reached the required # of inliers
     if num_inliers_best >= num_inliers_required
         break
@@ -228,36 +229,29 @@ end
 
 % optimization:
 a = [inliers_x1 inliers_y1];
-
 b = [inliers_x2 inliers_y2];
-
 imageSize = size(I);
 imageWidth = imageSize(1);
 imageLength = imageSize(2);
 
-%function
-%fun = @(a,b)symsum(abs(((h1'*a(i))/(h3'*a(i))) - ((h1'*a(i))/(h3'*a(i)))), i, 1, k);
-
+% function
 fun = @(d) rect_func_v(d,imageLength,imageWidth,a',b');
-
 [d,f] = fminsearch(fun,[0 0]);
- 
 [~,Hv] = rect_func_v(d,imageLength,imageWidth,[],[]);
-
 boundaries=Hv*[1 imageWidth imageWidth 1;1 1 imageLength imageLength;1 1 1 1];
 boundaries=boundaries./repmat(boundaries(3,:),[3 1]);
 x=boundaries(1,1);
-
 width_v=boundaries(1,2)+x;
 length_v=boundaries(2,3);
 
-
-%transform original lines 
+% transform original lines 
 L = Lall;
 tform = maketform('projective',Hv'); % 2d spatial transformation required
+
 % transforming image to affine using H:
 newImage = imtransform(handles.A, tform);
 
+% prepare Lv for horizontal perspective
 A=L(:,1:2)';B=L(:,3:4)';k=size(A,2);
 A_=Hv*[A;ones(1,k)];A_=A_./repmat(A_(3,:),[3 1]);
 B_=Hv*[B;ones(1,k)];B_=B_./repmat(B_(3,:),[3 1]);
@@ -268,63 +262,63 @@ Lv=[A_(1,:)-x;A_(2,:);B_(1,:)-x;B_(2,:)]';
 
 angle_threshold=20;
 
-%segment endpoints
+% segment endpoints
 X1=Lv(:,1); X2=Lv(:,3); Y1=Lv(:,2); Y2=Lv(:,4);
 XY=[X2-X1 Y2-Y1];
 
-%segment orientations
+% segment orientations
 lineangle =(atan(XY(:,2)./XY(:,1)))/pi*180;
-
 Lv(abs(lineangle)>(90-angle_threshold),:)=[];
-
 midpoints = (Lv(:,1:2)+Lv(:,3:4))/2;
 
-%segment endpoints
+% segment endpoints
 X1=Lv(:,1); X2=Lv(:,3); Y1=Lv(:,2); Y2=Lv(:,4);
 XY=[X2-X1 Y2-Y1];
 
-%segment orientations
+% segment orientations
 lineangle =(atan(XY(:,2)./XY(:,1)))/pi*180;
-%lineangle(lineangle<0)=lineangle(lineangle<0)+180;
 
+
+% RANSAC
 data = [midpoints(:,2)  lineangle]';
-
 iterations = 20; % k
 min_points = 2; % n
 threshold = 7; % t
 num_inliers_required_h = 140; % d
-
-% initialization
 num_inliers_best_h = 0;
 x = data(1,:);
 y = data(2,:);
 
-% main loop
 for i=1:iterations
-    
     % randomly select points from data (subset)
     [~,idx] = datasample(x,min_points);
+    
     % get coffecients
     coef = polyfit(x(1,idx),y(1,idx),1); % uses least squares
+    
     % get predictions of y from coefficients using polyval
     y_pred = polyval(coef,x);
+    
     % find square residual
     square_resid = (y - y_pred).^2;
+    
     % compare all points (using square residual) to threshold
     inliers_h = [x(square_resid < threshold); y(square_resid < threshold)];
     inliers_x1_h = X1(square_resid < threshold);
     inliers_x2_h = X2(square_resid < threshold);
     inliers_y1_h = Y1(square_resid < threshold);
     inliers_y2_h = Y2(square_resid < threshold);
+    
     % skip iteration if size of current inliers < size of best
     if length(inliers_h) < num_inliers_best_h
         continue;
     end
+    
     % current random sample is best sample if reached here
     best_inliers_h = inliers_h;
-    
     num_inliers_best_h = length(best_inliers_h);
     best_model = coef;
+    
     % break out of the loop if we reached the required # of inliers
     if num_inliers_best_h >= num_inliers_required_h
         break
@@ -334,29 +328,20 @@ end
 
 % optimization:
 a = [inliers_x1_h inliers_y1_h];
-
 b = [inliers_x2_h inliers_y2_h];
-
 fun = @(d) rect_func_h(d,length_v,width_v,a',b');
-
 options = optimset;
 options = optimset(options,'Display', 'off');
 options = optimset(options,'Algorithm', 'active-set');
-
 [d,f] = fmincon(fun,[0 0],[],[],[],[],[-4*imageLength -4*imageLength],[4*imageLength 4*imageLength],[],options);
-
 [~,Hh] = rect_func_h(d,imageLength,imageWidth,[],[]);
 
-
-%transform
+% transform
 tform = maketform('projective',Hh'); % 2d spatial transformation required
 % transforming image to affine using H:
 finalImage = imtransform(newImage, tform);
 
 imshow(finalImage);
-
-
-
 
 % --- Executes on button press in pushbutton6.
 function pushbutton6_Callback(hObject, eventdata, handles)
@@ -444,74 +429,114 @@ function pushbutton10_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton10 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% locate the pair of parallel lines:
-l1 = cross(handles.p1,handles.p2)
-l2 = cross(handles.p3,handles.p4);
-l3 = cross(handles.p2, handles.p3);
-l4 = cross(handles.p1, handles.p4);
+im = handles.A;
+p1 = handles.p1;
+p2 = handles.p2;
+p3 = handles.p3;
+p4 = handles.p4;
+
+l1 = cross(p1,p2)
+l2 = cross(p3,p4);
+l3 = cross(p2, p3);
+l4 = cross(p1, p4);
+
 % find the  line at infinity:
-infA = cross(l1, l2);
-infA = infA/infA(1,3); % first pair
-infB = cross(l3, l4);
-infB = infB/ infB(1,3); % second pair
-l = cross(infA, infB) % line at infinity
-% transformation matrix H, put homogeneous coordinates of line at infinity:
-l(1, 1);
-l(1, 2);
-l(1, 3);
-H = [1 0 0; 0 1 0; l(1, 1)/l(1,3) l(1, 2)/l(1,3) 1];
-tform = maketform('projective',H'); % 2d spatial transformation required
-% transforming image to affine using H:
-affineImage = imtransform(handles.A, tform);
-handles.p1'
-p1x = H* handles.p1'
+a = cross(l1, l2);
+a = a/a(1,3); % first pair
+b = cross(l3, l4);
+b = b/ b(1,3); % second pair
+
+l_inf = cross(a, b);
+l_inf=l_inf/l_inf(3);
+
+% Recover the projective transformation that maps the line at infinity to its canonical position
+P = [1,0,0;0,1,0;l_inf(1),l_inf(2),1];
+
+m1 = l4;
+m2 = l3;
+v1=cross(l1,l2);v1=v1/v1(3);
+v2=cross(m1,m2);v2=v2/v2(3);
+
+% Define constraints
+% contraint: a known right angle between two orthogonal lines
+p1x = P* handles.p1'
 p1x = [p1x(1)/p1x(3); p1x(2)/p1x(3); 1]
-%p1x = p1x';
-p2x = H* handles.p2'
+p2x = P* handles.p2'
 p2x = [p2x(1)/p2x(3); p2x(2)/p2x(3); 1]
-%p2x = p2x';
-p3x = H* handles.p3'
+p3x = P* handles.p3'
 p3x = [p3x(1)/p3x(3); p3x(2)/p3x(3); 1]
-%p3x = p3x';
-p4x = H* handles.p4'
+p4x = P* handles.p4'
 p4x = [p4x(1)/p4x(3); p4x(2)/p4x(3); 1]
-%p4x = p4x';
 
-% begin metric transformation
-% find l_a and l_b, define theta
-
-la_1 = cross(p1x,p2x);
-la_1 = [la_1(1)/la_1(3) la_1(2)/la_1(3) 1]
-
-lb_1 = cross(p4x,p1x);
-lb_1 = [lb_1(1)/lb_1(3) lb_1(2)/lb_1(3) 1]
-
-la_2 = cross(p2x,p3x);
-la_2 = [la_2(1)/la_2(3) la_2(2)/la_2(3) 1]
-
-lb_2 = cross(p3x,p4x);
-lb_2 = [lb_2(1)/lb_2(3) lb_2(2)/lb_2(3) 1]
-
-% compute variables a and b:
-a1 = -la_1(2)/la_1(1)
-a2 = -la_2(2)/la_2(1)
-b1 = -lb_1(2)/lb_1(1)
-b2 = -lb_2(2)/lb_2(1)
-% find radius and centers of circle
-r1 = abs(a1 - b1)/2
-r2 = abs(a2 - b2)/2
-c1_alpha = (a1 + b2)/2
-c2_alpha = (a2 + b1)/2
+la = cross(p1x,p2x);
+la = [la(1)/la(3) la(2)/la(3) 1]
+lb = cross(p2x,p3x);
+lb = [lb(1)/lb(3) lb(2)/lb(3) 1]
+a1 = -la(2)/la(1)
+b1 = -lb(2)/lb(1)
+rd1 = abs(a1 - b1)/2
+c1_alpha = (a1 + b1)/2
 c1_beta = 0;
-c2_beta = 0;
-% intersection of the two circles:
-[pt1, pt2] = circcirc(c1_alpha,c1_beta,r1,c2_alpha,c2_beta,r2)
-% pt1 is the point we need
-alpha = pt1(1);
-beta = pt1(2);
-% define transformation matrix:
-H = [(1/beta) -(alpha/beta) 0; 0 1 0; 0 0 1];
-tform = maketform('projective',H');
-metricImage = imtransform(affineImage, tform);
 
-imshow(metricImage);
+% contraint: angle between diagonals given known length ratio
+% calculate length ratio
+inter1 = cross(l1,m1); p1 = inter1/inter1(3);
+inter2 = cross(l2,m1); p2 = inter2/inter2(3);
+inter3 = cross(l1,m2); p3 = inter3/inter3(3);
+inter4 = cross(l2,m2); p4 = inter4/inter4(3);
+
+% estimate aspect ratio through rectangle vertices
+k2=dot((cross(p1,p4)),p3)/dot((cross(p2,p4)),p3);
+k3=dot((cross(p1,p4)),p2)/dot((cross(p3,p4)),p2);
+n2(:,1)=k2*p2-p1;
+n3(:,1)=k3*p3-p1;
+im_width=size(im,2);
+im_height=size(im,1);
+u0=im_width/2;
+v0=im_height/2;
+f=(-v1(1)*u0-v1(2)*v0-1)/(v1(1)*v1(2)+v2(1)*v2(2));
+A=[f 0 u0;0 f v0;0 0 1];
+s=(n2'*inv(A)'*(A\n2))/...
+    (n3'*inv(A)'*(A\n3));
+s=sqrt(s);
+
+% apply ratio to metric constraints
+ld1=cross(p1,p4);ld1=ld1/ld1(3);ld1A=ld1/P;
+ld2=cross(p2,p3);ld2=ld2/ld2(3);ld2A=ld2/P;
+theta=2*asin(1/sqrt(s^2+1));
+a2=-ld1A(2)/ld1A(1);b2=-ld2A(2)/ld2A(1);
+c2_alpha=(a2+b2)/2;
+c2_beta=((a2-b2)/2)*cot(theta);
+rd2=abs((a2-b2)/(2*sin(theta)));
+
+% Constraint combination
+if rd1>0 && rd2>0 &&rd1~=Inf && rd2~=Inf
+    [alpha,betta] = circcirc(c1_alpha,c1_beta,rd1,c2_alpha,c2_beta,rd2);
+    
+    % Estimation of the metric transformation
+    A=[1/abs(betta(1)) -alpha(1)/abs(betta(1)) 0;0 1 0;0 0 1];
+    
+    % Similarity transformation
+    l1S=l1/(A*P);l1S=l1S/l1S(3);
+    angle=atan(l1S(1)/l1S(2));
+
+    R = [cos(angle) -sin(angle); sin(angle) cos(angle)];
+    S = [R zeros(2,1);0 0 1];
+    
+    % Metrically rectified image
+    H=[4 0 0;0 4 0; 0 0 1]*S*A*P;
+else
+    H=eye(3);
+    s=-1;
+    disp('Warning: Homography = I');
+end
+
+p1_=H*p1';p1_=p1_/p1_(3);
+p2_=H*p2';p2_=p2_/p2_(3);
+p3_=H*p3';p3_=p3_/p3_(3);
+p4_=H*p4';p4_=p4_/p4_(3);
+R = makeresampler({'cubic','nearest'},'bound');
+Im_metr = imtransform(im,(maketform('projective',(H'))),R,'FillValues',[10;20;30],'XYScale',1);
+
+% show metric result
+imshow(Im_metr);
